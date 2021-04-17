a0bf1f3f2a67385461faba184d38fb9bb7ceb22f
jonel.agutaya <82687486+jaagut123@users.noreply.github.com>
2021-04-17 11:13:58 +0800

    Update main.js
    add calculate function

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+function calculate() {
+  let f = 60;
+  let B = getElementById("flux").value;
+  let w = getElementById("leg").value;
+  let t = getElementById("thick").value;
+  let A = w * t;
+  let TPV = calculateTunsPerVolt(f, B, A);
+  let priV = getElementById("pri-v").value;
+  let secV = getElementById("sec-v").value;
+  getElementById("pri-n).value = TPV * priV;
+  getElementById("sec-n).value = TPV * secV;
+}
+
 function calculateTurnsPerVolt(f, B, A) {
   // formula TPV = = 1/(k*f*B*A)
   // f = in Hertz, Tesla, square meter
