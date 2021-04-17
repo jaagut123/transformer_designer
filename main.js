e6064bbb813b76fcdf46b70f727c710ba733c12c
jonel.agutaya <82687486+jaagut123@users.noreply.github.com>
2021-04-17 13:02:07 +0800

    Update main.js
    Add conversion factor

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
+let prevUnit = "mm" // mm, cm, in
+
+function recalc() {
+  let newUnit = document.getElementById("unit").value;
+  let factor = 1;
+  if (prevUnit=="mm" && newUnit=="cm") factor = 1/10;
+  if (prevUnit=="mm" && newUnit=="in") factor = 1/25.4;
+  if (prevUnit=="cm" && newUnit=="mm") factor = 10;
+  if (prevUnit=="cm" && newUnit=="in") factor = 1/2.54;
+  if (prevUnit=="in" && newUnit=="mm") factor = 25.4;
+  if (prevUnit=="in" && newUnit=="cm") factor = 2.54;
+  document.getElementById("leg").value *= factor; 
+  document.getElementById("thick").value *=  factor; 
+  
+}
+
 function calculate() {
-  let f = 60;
+  let f = document.getElementById("freq").value;
   let B = document.getElementById("flux").value; // Tesla
-  let w = document.getElementById("leg").value; // mm
-  let t = document.getElementById("thick").value; // mm
-  let A = w * t / 1000000; // square meters
+  let w = document.getElementById("leg").value;
+  let t = document.getElementById("thick").value;
+  let factor = 1; // factor to convert unit to meters
+  let newUnit = document.getElementById("unit").value;
+  if (newUnit=="mm") factor = 1/1000; // mm to m
+  if (newUnit=="cm") factor = 1/100; // cm to m
+  if (newUnit=="in") factor = 2.54/100; // in to m
+  let A = w * t * factor * factor; // square meters
   let TPV = calculateTurnsPerVolt(f, B, A);
   let priV = document.getElementById("pri-v").value;
   let secV = document.getElementById("sec-v").value;
